d8dabdd26f0bc261aaa90602fb45ead24625a44c
Wim Suenens <113470379+Oribi4WS@users.noreply.github.com>
2022-09-15 22:31:39 +0200

    update zsh setup

--- zsh.setup.zsh
+++ zsh.setup.zsh
@@ -1,15 +1,29 @@
 #!/usr/bin/env zsh
 
-echo "\n <<< Setup ZSH >>>>\n"
+echo "\n --- Setup ZSH --- \n"
 
-# if exists brew; then
-#     echo "Great! Homebrew is already installed!\n"
-# else
-#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# fi
+# if [ $(cat /etc/shells | grep "/opt/homebrew/bin/zsh") ]; then
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+    echo "/opt/homebrew/bin/zsh has already been added to /etc/shells\n"
+else
+    echo "Enter superuser (sudo) password to edit /etc/shells"
+    echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+    echo "\n"
+fi
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if [ $SHELL = '/opt/homebrew/bin/zsh' ]; then
+    echo "Great! The login shell is /opt/homebrew/bin/zsh."
+else
+    echo "Enter user password to change the login shell"
+    chsh -s '/opt/homebrew/bin/zsh'
+fi
 
-echo "Enter user password to change the login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+if sh --version | grep -q zsh; then
+    echo '/private/var/select/sh already linked to /bin/zsh'
+else
+    echo "Enter superuser (sudo) password to symlink sh to zsh"
+    # man sh => sh is a POSIX-compliant command interpreter (shell).  It is implemented by re-execing as either bash(1), dash(1), or zsh(1) as determined by the symbolic link located at /private/var/select/sh.
+    sudo ln -sfv /bin/zsh /private/var/select/sh
+    # I prefer this instead, but not working as /opt/homebrew/bin/zsh is a symlink...
+    # sudo ln -sfv /opt/homebrew/bin/zsh /private/var/select/sh
+fi
